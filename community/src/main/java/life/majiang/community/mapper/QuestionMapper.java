259837b6d3b659b746f379e26ca113e71b27467b
hai <1098866704@qq.com>
2020-02-09 10:04:15 +0800

    创建项目

---
+++ community/src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -0,0 +1,36 @@
+package life.majiang.community.mapper;
+
+import java.util.List;
+import life.majiang.community.pojo.Question;
+import life.majiang.community.pojo.QuestionExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface QuestionMapper {
+    long countByExample(QuestionExample example);
+
+    int deleteByExample(QuestionExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Question record);
+
+    int insertSelective(Question record);
+
+    List<Question> selectByExampleWithBLOBs(QuestionExample example);
+
+    List<Question> selectByExample(QuestionExample example);
+
+    Question selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Question record, @Param("example") QuestionExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Question record, @Param("example") QuestionExample example);
+
+    int updateByExample(@Param("record") Question record, @Param("example") QuestionExample example);
+
+    int updateByPrimaryKeySelective(Question record);
+
+    int updateByPrimaryKeyWithBLOBs(Question record);
+
+    int updateByPrimaryKey(Question record);
+}
